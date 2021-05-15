09fce0199c545cc35f8a37f7d0be788ad370844b
ChampKatika <inform.katika@gmail.com>
2021-05-15 11:54:05 +0700

    update cors

--- app/app/main.py
+++ app/app/main.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_restful import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+
 #
 from .api.routes import initialize_routes
 from .api.db import initialize_db
@@ -16,16 +18,20 @@ app.config['MONGODB_SETTINGS'] = {
     # 'password':'example'
     'host': 'mongodb://root:example@localhost:27017/mydb?authSource=admin'
 }
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 app.config.from_envvar('ENV_FILE_LOCATION')
+CORS(app, resources=r'/api/*')
+#resources={r"/api/*": {"origins": "*", "allow_headers": "*", "expose_headers": "*", supports_credentials: True}})
 
 api = Api(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
+
+
 initialize_db(app)
 initialize_routes(api)
-
 
 if __name__ == "__main__":
     # Only for debugging while developing
